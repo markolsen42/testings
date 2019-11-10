e6e603ad2a1229162c7b650ec5b68b52f694d77b
Mark Olsen <markol@datacom.co.nz>
2019-11-11 09:32:48 +1300

    add a file that can be used from the main file

--- testings.go
+++ testings.go
@@ -1,22 +1,25 @@
-package main 
+package main
 
-import( "fmt"
-"strings")
+import (
+	"fmt"
+	"strings"
+)
 
 func findin(str string, h string) int {
-  return strings.Index(h, str)
+	return strings.Index(h, str)
 }
+
 //next thing to work on
 func findReplaceText(str string, h string) string {
-	var splits = strings.Split(h, str);
+	var splits = strings.Split(h, str)
 	fmt.Print(splits)
 	return splits[1]
 }
 
-
 func main() {
 
-var html = "jfkasdhfkjhsdafkj***insert***ldkfsajsdlkfj***insert2***"
-  fmt.Print(findReplaceText("***",html))
+	var html = "jfkasdhfkjhsdafkj***insert***ldkfsajsdlkfj***insert2***"
+	fmt.Print(findReplaceText("***", html))
+	fmt.Print(GetName())
 
 }
